0393b331e54929fe8dbbad97cf049a33829674e5
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-31 09:52:35 +0200

    using api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import ToDo from "./ToDo.js";
+import { useEffect, useState } from "react";
 
 function App() {
-  const toDo = [{
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false,
-  }];
+  const [toDo, setToDo] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  const toDoJSX = toDo.map((oneThing, index) => {
-    return <ToDo key={index} {...oneThing}/>
-  });
+  useEffect(() => {
+
+    async function getToDo() {
+      setLoading(true);
+
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos/4");
+      const data = await response.json();
+      
+      setToDo(data);
+      setLoading(false);
+    }
+
+    getToDo();
+  }, []);
 
   return (
     <>
-      {toDoJSX}
+      {loading === false ? <ToDo {...toDo}/> : <p>No info to load..</p>}
     </>
   );
 }
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 function ToDo(props) {
     const [completed, setCompleted] = useState(props.completed);
